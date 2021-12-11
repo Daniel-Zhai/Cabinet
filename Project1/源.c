b8774764a7989cc1f67c77e459a61604057df81e
Daniel <2553182585@qq.com>
2021-12-11 23:46:54 +0800

    练习

---
+++ Project1/源.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int ret = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);
+//	while (getchar()!='\n');
+//	printf("请确认(Y/N):>");
+//	ret = getchar();
+//	if (ret == 'Y')
+//		printf("确认成功");
+//	else
+//		printf("放弃确认");
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch>'9')
+//		{
+//			continue;
+//		}
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
